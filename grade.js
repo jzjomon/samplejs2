ca1687962f2bfcb71ec139d0d2c1781bf62c93e8
jomon jose <jzjomon@gmail.com>
2023-05-11 21:31:15 +0530

    grade finished

---
+++ grade.js
@@ -0,0 +1,12 @@
+let i=51;
+if(i>90){
+    console.log("your grade is A");
+}else if(i>70){
+    console.log("your grade is B");
+}else if(i>50){
+    console.log("your grade is C");
+}else if(i>30){
+    console.log("your grade is D");
+}else{
+    console.log("your grade is E");
+}
